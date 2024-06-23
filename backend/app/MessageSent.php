587dbdbda1b029b7aa3228a851e50a422c02e2be
Michael-LinuxAdminAspiringDevSecOps <73505902+Golomichael@users.noreply.github.com>
2024-06-23 13:59:56 -0400

    Add files via upload

---
+++ backend/app/MessageSent.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $channel;
+
+    public function __construct($message, $channel)
+    {
+        $this->message = $message;
+        $this->channel = $channel;
+    }
+
+    public function broadcastOn()
+    {
+        return [$this->channel];
+    }
+
+    public function broadcastAs()
+    {
+        return 'MessageSent';
+    }
+}
